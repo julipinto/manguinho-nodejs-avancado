1afc2bc0e72526720f2352299560f33d47c9ba0a
Juliana <aragaopintojuli@gmail.com>
2025-01-12 15:36:31 -0300

    ensure Service calls LoadFacebookUserApi with correct params

--- jest.config.mjs
+++ jest.config.mjs
@@ -87,11 +87,12 @@ const config = {
   //   "json",
   //   "node"
   // ],
-
+  // "@/*": ["*"],
+  // "@/tests/*": ["../tests/*"],
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "@/tests/(.+)": "<rootDir>/tests/$1",
-    "@/(.+)": "<rootDir>/src/$1",
+    "@/tests/(.*)": "<rootDir>/tests/$1",
+    "@/(.*)": "<rootDir>/src/$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
