bd0332191bfdc0d0cde263a419e331acda6594fe
Juliana <aragaopintojuli@gmail.com>
2025-01-26 10:30:54 -0300

    fix to new version of typeorm and pg-mem

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,11 +1,13 @@
 import { LoadUSerAccountRepository } from "@/data/contracts/repos";
 
 import { newDb } from 'pg-mem';
-import { Column, Entity, getRepository, PrimaryGeneratedColumn } from "typeorm";
+import { Column, DataSource, Entity, getRepository, PrimaryGeneratedColumn } from "typeorm";
 
 class PgUserAccountRepository implements LoadUSerAccountRepository {
+  constructor(private readonly connection: DataSource) {}
+
   async load(params: LoadUSerAccountRepository.Params): Promise<LoadUSerAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
+    const pgUserRepo = this.connection.getRepository(PgUser);
     const pgUser = await pgUserRepo.findOneBy({ email: params.email });
     if (!!pgUser) {
       return {
@@ -35,24 +37,69 @@ describe('PgUserAccountRepository', () => {
   describe('load', () => {
     it('should return an account if email exists', async () => {
       const db = newDb();
-      const connection = await db.adapters.createTypeormDataSource ({
-          type: 'postgres',
-          entities: [PgUser]
-      })
 
-      // create schema
-      await connection.synchronize();
-      const pgUserRepo = getRepository(PgUser)
+      db.public.registerFunction({
+        name: 'current_database',
+        implementation: () => 'test_db',
+      });
 
+      db.public.registerFunction({
+        name: 'version',
+        implementation: () => 'PostgreSQL 14.0 (pg-mem)',
+      });
+
+
+      const connection = await db.adapters.createTypeormDataSource({
+        type: 'postgres',
+        entities: [PgUser],
+        synchronize: true,
+      });
+
+      await connection.initialize();
+
+      const pgUserRepo = connection.getRepository(PgUser);
       await pgUserRepo.save({ email: 'existing_email' });
 
-      const sut = new PgUserAccountRepository();
-
+      const sut = new PgUserAccountRepository(connection);
       const account = await sut.load({ email: 'existing_email' });
 
       expect(account).toEqual({
         id: '1',
-      })
+      });
+
+      await connection.destroy();
+    });
+
+    it('should return undefined if email does not exists', async () => {
+      const db = newDb();
+
+      db.public.registerFunction({
+        name: 'current_database',
+        implementation: () => 'test_db',
+      });
+
+      db.public.registerFunction({
+        name: 'version',
+        implementation: () => 'PostgreSQL 14.0 (pg-mem)',
+      });
+
+      const connection = await db.adapters.createTypeormDataSource({
+        type: 'postgres',
+        entities: [PgUser],
+        synchronize: true,
+      });
+
+      await connection.initialize();
+
+      const sut = new PgUserAccountRepository(connection);
+
+      const account = await sut.load({ email: 'new_email' });
+
+      expect(account).toBeUndefined();
+
+      await connection.close();
     });
   });
 });
+
+
