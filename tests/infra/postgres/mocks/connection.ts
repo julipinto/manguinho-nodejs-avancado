f4b1dfd1bfe29f25e10ff5cccb4dd94e93388cd2
Juliana <aragaopintojuli@gmail.com>
2025-01-26 11:22:07 -0300

    move inMemory db creation to its own file

---
+++ tests/infra/postgres/mocks/connection.ts
@@ -0,0 +1,28 @@
+import { IMemoryDb, newDb } from "pg-mem";
+import { DataSource } from "typeorm";
+
+export const makeFakeDb = async (entities?: any[]): Promise<{db: IMemoryDb, connection: DataSource}> => {
+  let connection: DataSource;
+
+  const db = newDb();
+
+  db.public.registerFunction({
+    name: 'current_database',
+    implementation: () => 'test_db',
+  });
+
+  db.public.registerFunction({
+    name: 'version',
+    implementation: () => 'PostgreSQL 14.0 (pg-mem)',
+  });
+
+  connection = await db.adapters.createTypeormDataSource({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/index.ts'],
+    synchronize: true,
+  });
+
+  await connection.initialize();
+
+  return { db, connection };
+}
--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -3,32 +3,8 @@ import { PgUserAccountRepository } from "@/infra/postgres/repos";
 
 import { IBackup, IMemoryDb, newDb } from 'pg-mem';
 import { DataSource, Repository } from "typeorm";
+import { makeFakeDb } from "@/tests/infra/postgres/mocks";
 
-const makeFakeDb = async (entities?: any[]): Promise<{db: IMemoryDb, connection: DataSource}> => {
-  let connection: DataSource;
-
-  const db = newDb();
-
-  db.public.registerFunction({
-    name: 'current_database',
-    implementation: () => 'test_db',
-  });
-
-  db.public.registerFunction({
-    name: 'version',
-    implementation: () => 'PostgreSQL 14.0 (pg-mem)',
-  });
-
-  connection = await db.adapters.createTypeormDataSource({
-    type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts'],
-    synchronize: true,
-  });
-
-  await connection.initialize();
-
-  return { db, connection };
-}
 
 describe('PgUserAccountRepository', () => {
   describe('load', () => {
