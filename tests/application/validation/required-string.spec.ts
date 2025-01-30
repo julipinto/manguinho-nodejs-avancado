0a4623b4deb29a87ebcf34aa4b6f47992045161b
Juliana <aragaopintojuli@gmail.com>
2025-01-30 09:04:01 -0300

    move RequiredStringValidator to its own file

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -1,16 +1,5 @@
-/* eslint-disable eslint-comments/require-description */
 import { RequiredFieldError } from "@/application/errors";
-
-class RequiredStringValidator {
-  constructor(
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate(): Error | undefined {
-    return new RequiredFieldError(this.fieldName)
-  }
-}
+import { RequiredStringValidator } from "@/application/validation";
 
 describe('RequiredStringValidator', () => {
   it('should return RequiredFieldError if value is empty', () => {
@@ -23,7 +12,6 @@ describe('RequiredStringValidator', () => {
   });
 
   it('should return RequiredFieldError if value is null', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-type-assertion, @typescript-eslint/no-explicit-any
     const sut = new RequiredStringValidator(null as any, 'any_field')
 
     const error = sut.validate()
@@ -32,11 +20,18 @@ describe('RequiredStringValidator', () => {
   });
 
   it('should return RequiredFieldError if value is undefined', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-type-assertion, @typescript-eslint/no-explicit-any
     const sut = new RequiredStringValidator(undefined as any, 'any_field')
 
     const error = sut.validate()
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
+  });
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   });
 });
